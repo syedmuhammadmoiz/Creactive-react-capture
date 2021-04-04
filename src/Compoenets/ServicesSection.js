8c9c867e57b9a2a667e194dda3ea807274e25225
syedmuhammadmoiz <moiz66.smm@gmail.com>
2021-04-04 06:47:13 +0500

    3. Services Section

---
+++ src/Compoenets/ServicesSection.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import clock from '../img/clock.svg'
+import diaphragm from '../img/diaphragm.svg'
+import money from '../img/money.svg'
+import teamwork from '../img/teamwork.svg'
+import home2 from '../img/home2.png'
+
+function ServicesSection() {
+    return (
+      <div className="services">
+          <div className="description">
+              <h2>High <span>quality</span>services</h2>
+              <div className="card">
+              <div className="cards">
+                  <div className="icon">
+                       <img src={clock} alt="clock"/>
+                       <h3>Efficient</h3>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="cards">
+                  <div className="icon">
+                       <img src={teamwork} alt="treamwork"/>
+                       <h3>Teamwork</h3>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="cards">
+                  <div className="icon">
+                       <img src={diaphragm} alt="diaphragm"/>
+                       <h3>Diapgragm</h3>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="cards">
+                  <div className="icon">
+                       <img src={money} alt="money"/>
+                       <h3>mONEY</h3>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+          </div>
+        </div>
+        <div className="image">
+            <img src={home2} alt="camera"/>
+        </div>
+      </div>
+    )
+}
+
+export default ServicesSection
--- src/pages/AboutsUs.js
+++ src/pages/AboutsUs.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import AboutSection from '../Compoenets/AbountSection'
+import ServicesSection from '../Compoenets/ServicesSection'
 
 function AboutsUs() {
     return (
+        <>
        <AboutSection/>
+       <ServicesSection/>
+       </>
     )
 }
 
