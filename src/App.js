3b6cb5c39ec31d670b5903bbfc5d6470615ff027
syedmuhammadmoiz <moiz66.smm@gmail.com>
2021-04-04 07:49:42 +0500

    5. Intro to Styles-Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AboutUs from './pages/AboutsUs'
+import GlobalStyle from './Compoenets/GlobalStyle'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
     <AboutUs/>
     </div>
   );
--- src/Compoenets/AbountSection.js
+++ src/Compoenets/AbountSection.js
@@ -1,28 +1,61 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
 
 function AbountSection() {
     return (
-        <div>
-            <div className="discription">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>Your <span>dream</span>come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
+                        <h2>Your <span>dream</span> come</h2>
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for photographt and videoagraphy and anything we will help youe get your goal.</p>
-            </div>
-            <div className="image">
+                <button>Contact Us</button>
+            </Description>
+            <Image>
                 <img src={home1} alt="Guy with a camera"/>
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
+
+const About = styled.div`
+min-height: 90vh;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 5rem 10rem;
+color: white;
+`;
+
+
+const Description = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2{
+    font-weight: lighter;
+}
+`;
+const Image = styled.div`
+flex:1;
+overflow: hidden;
+img{
+    width:100%;
+    height: 80vh;
+    object-fit: cover;
+}
+`;
+
+const Hide = styled.div `
+overflow: hidden;
+`;
 
 export default AbountSection
