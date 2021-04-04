584f3c5fdb88b3b70e36c458f3d45a1038b8ceb4
syedmuhammadmoiz <moiz66.smm@gmail.com>
2021-04-04 06:54:37 +0500

    4. Faq Section

--- src/pages/AboutsUs.js
+++ src/pages/AboutsUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../Compoenets/AbountSection'
 import ServicesSection from '../Compoenets/ServicesSection'
+import FaqSection from '../Compoenets/FaqSection'
 
 function AboutsUs() {
     return (
         <>
        <AboutSection/>
        <ServicesSection/>
+       <FaqSection/>
        </>
     )
 }
